f683020fc1d6875660a90791b5f05dea777380e5
Yannis <52321382+yannis300307@users.noreply.github.com>
2026-04-11 02:42:27 +0200

    feat(Refactor): Clean up and optimise the code. Add docs

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include "Geode/cocos/base_nodes/CCNode.h"
 #include "Geode/cocos/cocoa/CCGeometry.h"
-#include "Geode/cocos/layers_scenes_transitions_nodes/CCLayer.h"
-#include "Geode/cocos/menu_nodes/CCMenu.h"
-#include "Geode/cocos/platform/win32/CCGL.h"
 #include "Geode/cocos/sprite_nodes/CCSprite.h"
 #include "Geode/loader/Log.hpp"
 #include "fmod.hpp"
@@ -12,41 +9,11 @@
 #include <algorithm>
 #include <cmath>
 #include <numbers>
+#include <Geode/modify/MenuLayer.hpp>
 
 
 using namespace geode::prelude;
 
-#include <Geode/modify/MenuLayer.hpp>
-
-
-float getCurrentMusicVolume() {
-    auto engine = FMODAudioEngine::sharedEngine();
-    
-    FMOD::ChannelGroup* channel = engine->m_backgroundMusicChannel;
-    if (!channel) return 0.0f;
-
-    FMOD::DSP* dsp;
-    if (channel->getDSP(FMOD_CHANNELCONTROL_DSP_TAIL, &dsp) != FMOD_OK) {
-        return 0.0f;
-    }
-
-    dsp->setMeteringEnabled(true, false);
-
-    FMOD_DSP_METERING_INFO info;
-    if (dsp->getMeteringInfo(&info, nullptr) != FMOD_OK) {
-        return 0.0f;
-    }
-
-    float rms = 0.0f;
-    if (info.numchannels > 0) {
-        for (int i = 0; i < info.numchannels; i++) {
-            rms += info.rmslevel[i] * info.rmslevel[i];
-        }
-        rms = std::sqrt(rms / info.numchannels);
-    }
-
-    return rms;
-}
 
 const unsigned int BEATS_AVERGAGE = 2;
 const float BEAT_WIDTH = 0.12f;
@@ -67,30 +34,33 @@ class $modify(MyMenuLayer, MenuLayer) {
     };
 
 	bool init() {
+		// Call the default function
 		if (!MenuLayer::init()) {
 			return false;
 		}
 		
 		auto main_menu = this->getChildByID("main-menu");
-
 		auto play_button = main_menu->getChildByID("play-button");
 		auto sprite = play_button->getChildByIndex(0);
 
+		// Remove the default sprite for the play button
 		play_button->removeAllChildren();
 
+		// Replace the sprite with our round version of the button
 		auto new_sprite = CCSprite::create("play_button.png"_spr);
 		new_sprite->setScale(BASE_PLAY_BUTTON_SCALE);
 		new_sprite->setPosition({play_button->getContentWidth() / 2.0f, play_button->getContentHeight() / 2.0f});
 		play_button->setZOrder(10);
 		play_button->addChild(new_sprite);
 
-
-		auto beats_menu = CCNode::create();
-		beats_menu->setPosition({main_menu->getContentWidth() / 2.0f, main_menu->getContentHeight() / 2.0f});
-		beats_menu->setID("beats-menu");
-		beats_menu->setAnchorPoint({0.5f, 0.5f});
-		beats_menu->setZOrder(-1);
+		// Setup the beats nodes pool
+		auto beats_pool = CCNode::create();
+		beats_pool->setPosition({main_menu->getContentWidth() / 2.0f, main_menu->getContentHeight() / 2.0f});
+		beats_pool->setID("beats-menu");
+		beats_pool->setAnchorPoint({0.5f, 0.5f});
+		beats_pool->setZOrder(-1);
 		
+		// Create the beats
 		for (int i = 0; i < 360; i += BEATS_AVERGAGE)
 		{
 			float angle = i * std::numbers::pi / 180.f;
@@ -103,24 +73,32 @@ class $modify(MyMenuLayer, MenuLayer) {
 			sprite->setAnchorPoint({0.0f, 0.5f});
 			sprite->setRotation(-static_cast<float>(i));
 			sprite->setOpacity(200);
-			beats_menu->addChild(sprite);
+			beats_pool->addChild(sprite);
 		}
-
-		main_menu->addChild(beats_menu);
+		main_menu->addChild(beats_pool);
 
 		this->updateLayout();
 
+		// Reset the amplitude values
 		std::fill(this->m_fields->beats_amplitude, this->m_fields->beats_amplitude + BEATS_AMOUNT, 0.0f);
 
+		// Setup the DSPs for volume and spectrum analysis
 		auto engine = FMODAudioEngine::sharedEngine();
-        auto system = engine->m_system; // The core FMOD system
+        auto system = engine->m_system;
         FMOD::ChannelGroup* channel = engine->m_backgroundMusicChannel;
-
+		
+		// Spectrum
         if (channel && system) {
             system->createDSPByType(FMOD_DSP_TYPE_FFT, &m_fields->m_fftDSP);
             m_fields->m_fftDSP->setParameterInt(FMOD_DSP_FFT_WINDOWSIZE, SAMPLES_COUNT);
             channel->addDSP(FMOD_CHANNELCONTROL_DSP_TAIL, m_fields->m_fftDSP);
         }
+
+		// Volume
+		if (channel->getDSP(FMOD_CHANNELCONTROL_DSP_TAIL, &m_fields->m_fftDSP) == FMOD_OK) {
+			m_fields->m_fftDSP->setMeteringEnabled(true, false);
+		}
+
 
 		this->schedule(schedule_selector(MyMenuLayer::function_not_already_used));
 
@@ -128,24 +106,21 @@ class $modify(MyMenuLayer, MenuLayer) {
 	}
 	
 	void function_not_already_used(float dt) {
-		auto level = getCurrentMusicVolume();
-
-		auto main_menu = this->getChildByID("main-menu");
-		auto play_button = main_menu->getChildByID("play-button");
-		auto sprite = play_button->getChildByIndex(0);
-
-		auto beats_menu = main_menu->getChildByID("beats-menu");
-
-		//geode::log::debug("dt: {}", dt);
-
-		this->m_fields->beats_offset += dt * OFFSET_ROTATION_SPEED;
-		if (this->m_fields->beats_offset > SAMPLES_COUNT)
-		{
-			this->m_fields->beats_offset = 0.0f;
+		// Get the volume
+		FMOD_DSP_METERING_INFO info;
+		if (m_fields->m_fftDSP->getMeteringInfo(&info, nullptr) != FMOD_OK) {
+			return ;
 		}
 
-		sprite->setScale(BASE_PLAY_BUTTON_SCALE + level * 0.01f);
+		float volume = 0.0f;
+		if (info.numchannels > 0) {
+			for (int i = 0; i < info.numchannels; i++) {
+				volume += info.rmslevel[i] * info.rmslevel[i];
+			}
+			volume = std::sqrt(volume / info.numchannels);
+		}
 
+		// Get the spectrum
 		if (!m_fields->m_fftDSP) return;
 
 		FMOD_DSP_PARAMETER_FFT* fftData;
@@ -157,13 +132,31 @@ class $modify(MyMenuLayer, MenuLayer) {
 			for (int i = 0; i < SAMPLES_COUNT; i++) {
 				unsigned int ampl_index = (i - static_cast<int>(this->m_fields->beats_offset)) % SAMPLES_COUNT;
 				float amplitude = fftData->spectrum[0][ampl_index];
-				amplitude = powf(amplitude, 1.0f/3.0f);
+				amplitude = powf(amplitude, 1.0f / 3.0f);
 				float old_amplitude = this->m_fields->beats_amplitude[i];
 				if (amplitude > old_amplitude)
 					this->m_fields->beats_amplitude[i] = amplitude;	
 			}
 		}
 
+		// Rotate the beats
+		this->m_fields->beats_offset += dt * OFFSET_ROTATION_SPEED;
+		if (this->m_fields->beats_offset > SAMPLES_COUNT)
+		{
+			this->m_fields->beats_offset = 0.0f;
+		}
+
+		// Fetch the nodes
+		auto main_menu = this->getChildByID("main-menu");
+		auto play_button = main_menu->getChildByID("play-button");
+		auto sprite = play_button->getChildByIndex(0);
+
+		auto beats_menu = main_menu->getChildByID("beats-menu");
+
+		// Scale the play button based on the music volume
+		sprite->setScale(BASE_PLAY_BUTTON_SCALE + volume * 0.01f);
+
+		// Edit the lenght of the beats based on the spectrum
 		for (int i = 0; i < BEATS_AMOUNT; i++)
 		{
 			unsigned int ampl_index = i % SAMPLES_COUNT;
